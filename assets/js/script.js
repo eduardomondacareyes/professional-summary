ab8902ad336096b5cd5496ee0481d84efb860cff
Eduardo M <37560759+eduardomondacareyes@users.noreply.github.com>
2021-10-16 17:34:04 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(function() {
+    $("a").click(function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var gato = this.hash;
+
+
+            $('html, body').animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function() {
+
+                window.location.hash = gato;
+            });
+        } // Fin del if
+    });
+});
+
+
+$(function () {
+    $('[data-bs-toggle="popover"]').popover()
+  })
+
+
+
+
+
